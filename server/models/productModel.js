2737194acd85aa8346e759e304f2855971fa7b8f
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2021-05-21 11:26:14 +0700

    Create the models

---
+++ server/models/productModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+
+  ingredients: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  liked: {
+    type: Number,
+    required: true,
+  },
+  cal: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
